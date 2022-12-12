9eb375ee2df74da8e6bbadc9371e8a67eda4b3d6
Maxi Crimi <maxicrini@gmail.com>
2022-12-12 17:55:45 -0300

    Maximiliano Crimi

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import CartWidget from './CartWidget'
 
 const NavBar = () => {
     return (
         <div className="navbar bg-base-100 ">
-            <div className="flex-1">
+            <div className="flex-1"> 
+            <Link to={'/'}>
                 <a className="btn btn-ghost normal-case text-xl text-orange-500">Dragon Ball Tienda</a>
+            </Link>    
             </div>
             <div className= " pr-96">
                 <ul className='flex-column'>
-                    <li className="btn btn-ghost normal-case text-xl "><a>Saiajin</a></li>
-                    <li className="btn btn-ghost normal-case text-xl "><a>Freezer</a></li>
-                    <li className="btn btn-ghost normal-case text-xl "><a>Cell</a></li>
-                    <li className="btn btn-ghost normal-case text-xl "><a>Majin Boo</a></li>
+                    <Link to={'/category/Saiajin'}>
+                        <li className="btn btn-ghost normal-case text-xl "><a>Saiajin</a></li>
+                    </Link>
+                    <Link to={'/category/Freezer'}>
+                        <li className="btn btn-ghost normal-case text-xl "><a>Freezer</a></li>
+                    </Link>
+                    <Link to={'/category/Cell'}>
+                        <li className="btn btn-ghost normal-case text-xl "><a>Cell</a></li>
+                    </Link>
+                    <Link to={'/category/Majin Boo'}>
+                        <li className="btn btn-ghost normal-case text-xl "><a>Majin Boo</a></li>
+                    </Link>
                 </ul>
             </div>
             <CartWidget/>
